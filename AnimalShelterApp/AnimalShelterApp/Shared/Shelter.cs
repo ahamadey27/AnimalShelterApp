f0dbea79209c98910aab9b5bc1956a9f693e30d1
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-14 19:10:40 -0400

    Add initial Shelter class definition and JSON settings file and created shared folder with Shelter.cs class

---
+++ AnimalShelterApp/AnimalShelterApp/Shared/Shelter.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalShelterApp.Shared
+{
+    /// <summary>
+    /// Represents a single shelter or organization using the application.
+    /// This will be a top-level collection in Firestore.
+    public class Shelter
+    {
+        // The unique identifier for the shelter, mapping to the Firestore document ID.
+        public string Id { get; set; }
+
+        // The official name of the shelter.
+        public string Name { get; set; }
+
+        // The physical address of the shelter.
+        public string Address { get; set; }
+
+    }
+}
