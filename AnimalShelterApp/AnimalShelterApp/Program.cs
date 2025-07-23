17d70f4676eedd4d5fcbb58bf756813f35f539eb
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-23 14:57:54 -0400

    Refactor Program.cs to enhance authentication service registration and improve code organization

--- AnimalShelterApp/AnimalShelterApp/Program.cs
+++ AnimalShelterApp/AnimalShelterApp/Program.cs
@@ -1,16 +1,29 @@
 using AnimalShelterApp;
-using AnimalShelterApp.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using System.ComponentModel.DataAnnotations;
+using AnimalShelterApp.Services;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Configure the HttpClient for the app
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<AuthService>();
+
+// Register FirestoreService
 builder.Services.AddScoped<FirestoreService>();
+
+// ** START: Added Authentication Services **
+// Add core authorization services
+builder.Services.AddAuthorizationCore();
+
+// Register our custom AuthService as the implementation for AuthenticationStateProvider
+builder.Services.AddScoped<AuthenticationStateProvider, AuthService>();
+
+// Register AuthService so it can be injected into other components/services
+builder.Services.AddScoped<AuthService>(sp => (AuthService)sp.GetRequiredService<AuthenticationStateProvider>());
+// ** END: Added Authentication Services **
 
 await builder.Build().RunAsync();
 
